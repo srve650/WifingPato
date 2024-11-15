bf10ad459a18f89fb644dc03998fd34898fff16b
Joel A Marinay Jr <66583724+joel2128@users.noreply.github.com>
2024-11-16 00:56:08 +0800

    Update simplier.ps1

--- simplier.ps1
+++ simplier.ps1
@@ -275,3 +275,15 @@ for ($step = 1; $step -le $totalSteps; $step++) {
         4 { }
     }
 }
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
